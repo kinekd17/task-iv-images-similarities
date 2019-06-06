33ee1ad06d24b06e8abc2b918046d87a9dc4b19d
kropczynsky <kropaczmarcin@gmail.com>
2019-06-06 13:43:32 +0200

    8 point checkpoint

--- __main__.py
+++ __main__.py
@@ -7,33 +7,39 @@ from src.ransac import ransac_filter, get_best_model
 
 IMAGES_EXTENSION = '.png'
 IMAGES_DIR_PATH = 'images\\raw\\'
-# RANSAC
-MAX_ERROR = 0.9
-TRANSFORMATION = 'affine'
-NUMBER_OF_ITERATIONS = 100
 
+# NEIGHBOURHOOD INTEGRITY
+NEIGHBOURHOOD_SIZE = 23
+MINIMAL_INTEGRITY_POINT = 19
+
+# RANSAC
+MAX_ERROR = 100
+TRANSFORMATION = 'perspective'
+NUMBER_OF_ITERATIONS = 10000
+RANSAC = True
 
 if __name__ == '__main__':
-    object_name = 'book'
+    object_name = 'unicum'
     first_image_matrix, second_image_matrix = load_images_matrices(object_name)
     pairs = get_matches(first_image_matrix, second_image_matrix)
 
+    print("Potential pairs:", len(pairs))
     first_image_path = IMAGES_DIR_PATH + object_name + str(1) + IMAGES_EXTENSION
     second_image_path = IMAGES_DIR_PATH + object_name + str(2) + IMAGES_EXTENSION
 
-    # show_pairs(first_image_path=first_image_path,
-    #            second_image_path=second_image_path,
-    #            pairs=pairs)
+    if RANSAC:
+        model = get_best_model(pairs=pairs,
+                               transformation=TRANSFORMATION,
+                               max_error=MAX_ERROR,
+                               number_of_iterations=NUMBER_OF_ITERATIONS)
 
-    # pairs = neighbour_integrity_filter(pairs, 30, 10)
-    model = get_best_model(pairs=pairs,
-                           transformation=TRANSFORMATION,
-                           max_error=MAX_ERROR,
-                           number_of_iterations=NUMBER_OF_ITERATIONS)
+        pairs = ransac_filter(model=model,
+                              pairs=pairs,
+                              max_error=MAX_ERROR)
+    else:
+        pairs = neighbour_integrity_filter(pairs, NEIGHBOURHOOD_SIZE, MINIMAL_INTEGRITY_POINT)
 
-    pairs = ransac_filter(model=model,
-                          pairs=pairs,
-                          max_error=MAX_ERROR)
+    print("Confirmed pairs:", len(pairs))
 
     show_pairs(first_image_path=first_image_path,
                second_image_path=second_image_path,
--- src/transformations.py
+++ src/transformations.py
@@ -1,20 +1,23 @@
 import numpy as np
 import random
 import copy
-
+from pprint import pprint
 
 def get_affine_transformation(pairs):
     pairs = copy.deepcopy(pairs)
     random.shuffle(pairs)
     pairs = pairs[:3]
+    # pprint(pairs)
+    # pprint(pairs[0])
+    # pprint(pairs[0][0])
 
-    x1, y1 = pairs[0]
-    x2, y2 = pairs[1]
-    x3, y3 = pairs[2]
+    x1, y1 = pairs[0][0][0], pairs[0][0][1]
+    x2, y2 = pairs[1][0][0], pairs[1][0][1]
+    x3, y3 = pairs[2][0][0], pairs[2][0][1]
 
-    u1, v1 = x1, y1
-    u2, v2 = x2, y2
-    u3, v3 = x3, y3
+    u1, v1 = pairs[0][1][0], pairs[0][1][1]
+    u2, v2 = pairs[1][1][0], pairs[1][1][1]
+    u3, v3 = pairs[2][1][0], pairs[2][1][1]
 
     M = [[x1, y1,   1,  0,  0, 0],
          [x2, y2,   1,  0,  0, 0],
@@ -22,6 +25,7 @@ def get_affine_transformation(pairs):
          [0,   0,   0, x1, y1, 1],
          [0,   0,   0, x2, y2, 1],
          [0,   0,   0, x3, y3, 1]]
+
 
     N = [u1, u2, u3, v1, v2, v3]
 
@@ -41,15 +45,15 @@ def get_perspective_transformation(pairs):
     random.shuffle(pairs)
     pairs = pairs[:4]
 
-    x1, y1 = pairs[0]
-    x2, y2 = pairs[1]
-    x3, y3 = pairs[2]
-    x4, y4 = pairs[3]
+    x1, y1 = pairs[0][0][0], pairs[0][0][1]
+    x2, y2 = pairs[1][0][0], pairs[1][0][1]
+    x3, y3 = pairs[2][0][0], pairs[2][0][1]
+    x4, y4 = pairs[3][0][0], pairs[3][0][1]
 
-    u1, v1 = x1, y1
-    u2, v2 = x2, y2
-    u3, v3 = x3, y3
-    u4, v4 = x4, y4
+    u1, v1 = pairs[0][1][0], pairs[0][1][1]
+    u2, v2 = pairs[1][1][0], pairs[1][1][1]
+    u3, v3 = pairs[2][1][0], pairs[2][1][1]
+    u4, v4 = pairs[3][1][0], pairs[3][1][1]
 
     M = [[x1, y1, 1, 0, 0, 0, -u1*x1, -u1*y1],
          [x2, y2, 1, 0, 0, 0, -u2*x2, -u2*y2],
@@ -59,6 +63,7 @@ def get_perspective_transformation(pairs):
          [0, 0, 0, x2, y2, 1, -v2*x2, -v2*y2],
          [0, 0, 0, x3, y3, 1, -v3*x3, -v3*y3],
          [0, 0, 0, x4, y4, 1, -v4*x4, -v4*y4]]
+
 
     N = [u1, u2, u3, u4, v1, v2, v3, v4]
 
